b2325a078c83fa47e2c860b06e60c88fdfd8050f
Georgia Mills <georgee_123@hotmail.com>
2016-07-18 14:58:44 +0100

    cat image in local host

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+  'Hello!'
+end
+
+get '/secret' do
+  "secret text"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'
+  </div>"
+end
+
+get '/work' do
+  "work"
+end
